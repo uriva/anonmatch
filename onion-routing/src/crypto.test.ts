b0daeb8db5073851433024de5ddeaff43e12030a
uri <uri.valevski@gmail.com>
2023-02-22 16:58:53 +0200

    add test

--- onion-routing/src/crypto.test.ts
+++ onion-routing/src/crypto.test.ts
@@ -1,11 +1,15 @@
 import {
+  assert,
+  assertEquals,
+} from "https://deno.land/std@0.174.0/testing/asserts.ts";
+import {
   decryptAnonymously,
   encryptAnonymously,
   generatePrivateKey,
   getPublicKey,
+  sign,
+  verify,
 } from "./crypto.ts";
-
-import { assertEquals } from "https://deno.land/std@0.174.0/testing/asserts.ts";
 
 Deno.test("encrypt and decrypt", async () => {
   const secretKey = generatePrivateKey();
@@ -16,5 +20,17 @@ Deno.test("encrypt and decrypt", async () => {
       await encryptAnonymously(getPublicKey(secretKey), data),
     ),
     data,
+  );
+});
+
+Deno.test("sign and verify", async () => {
+  const secretKey = generatePrivateKey();
+  const data = "hello i am a message";
+  assert(
+    await verify(
+      getPublicKey(secretKey),
+      await sign(secretKey, data),
+      data,
+    ),
   );
 });
